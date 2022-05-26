a255bfdee264a54cb0130c8dbba793db2beb19cd
invalid-code <jessgaming55@gmail.com>
2022-05-26 17:34:00 +0800

    finished ability to edit blogs

--- mysite/my_blog/static/my_blog/blogs.js
+++ mysite/my_blog/static/my_blog/blogs.js
@@ -1,18 +1,23 @@
 $(document).ready(function () {
   $('#edit_blog').click(function () {
     let blog_title = $('#blog_title').text();
-    let blog_paragraph = $('#blog_paragraph_body').text();
-    console.log(blog_paragraph);
+    let blog_paragraph = $('#blog_paragraph_body').text().trim();
     $('#blog_title').replaceWith(
-      `<input value=${blog_title} name='blog_title'></input>`
+      `<input value=${blog_title} id='blog_title' name='blog_title' style='flex-grow: 1;'></input>`
     );
     $('#blog_paragraph_body').replaceWith(
-      `<textarea name='blog_paragraph_body' style="margin:10px">${blog_paragraph.trim()}</textarea>`
+      `<textarea id='blog_paragraph_body' name='blog_paragraph_body' style="margin: auto 0;">${blog_paragraph}</textarea>`
     );
     $('#cancel_button')
       .css({ display: 'block' })
       .click(function () {
-        $().replaceWith();
+        $('#blog_title').replaceWith(
+          `<div id="blog_title" name="blog_title" style='font-weight: 700; font-size: 1.25rem; line-height: 1.75rem; flex-grow: 1; text-align: right;'>${blog_title}</div>`
+        );
+        $('#blog_paragraph_body').replaceWith(
+          `<div id="blog_paragraph_body" name="blog_paragraph_body" style='margin-top: auto; margin-bottom: auto; text-align: right;'>${blog_paragraph}</div>`
+        );
+        $('#cancel_button').css({ display: 'none' });
       });
   });
 });
