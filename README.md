951e1a1ee59eec65e5db978657c922d635f657b7
invalid-code <jessgaming55@gmail.com>
2022-06-12 23:32:01 +0800

    added a way to comment if user is registered

--- README.md
+++ README.md
@@ -1,41 +1,4 @@
 My blog website
 
 server - django
-client - vue
 styling - tailwind
-
-# server
-
-cd mysite
-
-# frontend
-
-cd mysite/frontend
-
-## Project setup
-
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-
-```
-npm run build
-```
-
-### Lints and fixes files
-
-```
-npm run lint
-```
-
-### Customize configuration
-
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- mysite/my_blog/views.py
+++ mysite/my_blog/views.py
@@ -8,36 +8,55 @@ from .models import blog_collection, user_collection, user_comments
 user_counter = 0
 
 
-def comment():
-    return
-
+def comment(user_comment, commenter):
+    if commenter:
+        comment_content = {"comment": user_comment, "user": commenter}
+        user_comments.insert_one(comment_content)
+    else:
+        pass
 
 # Create your views here.
 def index(response):
-    blog_structure = list(blog_collection.find())
     if response.method == "POST":
-        pass
-    return render(response, "my_blog/index.html", {"blogs": blog_structure})
+        submit_comment = response.POST.get("comment_submit_button")
+        user_comment = response.POST.get("comment_input")
+        commenter = response.POST.get("comment_user")
+        logout_user = response.POST.get("logout_btn")
+        if submit_comment:
+            comment(user_comment=user_comment, commenter=commenter)
+        if logout_user:
+            pass
+    authenticated_user = response.session.get("user_information")
+    blogs = list(blog_collection.find())
+    comments = list(user_comments.find())
+    return render(response, "my_blog/index.html", {"blogs": blogs, "authenticated_user": authenticated_user, "comments": comments})
 
 
 def about_me(response):
-    return render(response, "my_blog/about_me.html", {})
-
+    if response.method == "POST":
+        submit_comment = response.POST.get("comment_submit_button")
+        user_comment = response.POST.get("comment_input")
+        commenter = response.POST.get("comment_user")
+        logout_user = response.POST.get("logout_btn")
+        if submit_comment:
+            comment(user_comment=user_comment, commenter=commenter)
+    authenticated_user = response.session.get("user_information")
+    comments = list(user_comments.find())
+    return render(response, "my_blog/about_me.html", {"comments": comments, "authenticated_user": authenticated_user})
 
 def blogs(response, blog_id):
-    blog_found = list(blog_collection.find({"_id": ObjectId(blog_id)}))
+    _id = {"_id": ObjectId(blog_id)}
     if response.method == "POST":
         save_blog = response.POST.get("save_blog")
         edit_blog_paragraph = response.POST.get("blog_paragraph_body")
         edit_blog_title = response.POST.get("blog_title")
         if save_blog:
             if edit_blog_paragraph and edit_blog_title:
-                _id = ObjectId(blog_id)
                 blog_updates = {"$set": {"title": edit_blog_title,
                                          "Paragraph_body": edit_blog_paragraph}}
-                blog_collection.update_one({"_id": _id}, blog_updates)
-    return render(response, "my_blog/blogs.html", {"blog_items": blog_found[0]})
-
+                blog_collection.update_one(_id, blog_updates)
+    found_blog = list(blog_collection.find(_id))
+    return render(response, "my_blog/blogs.html", {"blog_items": found_blog[0]})
 
 def create_blog(response):
     if response.method == "POST":
@@ -47,22 +66,28 @@ def create_blog(response):
         current_time = datetime.now()
         if new_blog:
             new_blog_form = {
-                "title": blog_body,
+                "title": blog_title,
                 "time": current_time.strftime("%m/%d/%Y %H:%M"),
-                "Paragraph_body": blog_title,
+                "Paragraph_body": blog_body,
             }
             blog_collection.insert_one(new_blog_form)
+            query_blog = {"title": blog_title, "Paragraph_body": blog_body}
+            blog_id = list(blog_collection.find(query_blog)).index(0).get("_id")
+            return redirect(f"blogs/{blog_id}/")
     return render(response, "my_blog/create_blog.html", {})
 
-
 def search(response):
-    search_term = None
+    search_result = None
     if response.method == "POST":
         search_key = response.POST.get("search_bar")
         if search_key:
-            search_term = list(blog_collection.find({"title": search_key}))
-    return render(response, "my_blog/search.html", {"search_items": search_term})
-
+            # query_term = {"title": search_key}
+            # search_result = list(blog_collection.find(query_term))
+            search_result = list(blog_collection.aggregate([{"$search": {"index": "blog_search_index", "text": {"query": search_key, "path": {"wildcard": "*"}}}}]))
+            if len(search_result) == 1:
+                blog_id = search_result[0].get("_id")
+                return redirect(f"/blogs/{blog_id}/")
+    return render(response, "my_blog/search.html", {"search_items": search_result})
 
 def sign_up(response):
     global user_counter
@@ -73,14 +98,13 @@ def sign_up(response):
         if register:
             user_counter += 1
             new_user = {
-            "username": username,
-            "password": make_password(password, salt=None, hasher="default"),
-            "user_count": user_counter,
+                "username": username,
+                "password": make_password(password, salt=None, hasher="default"),
+                "user_count": user_counter,
             }
-            # user_collection.insert_one(new_user)
+            user_collection.insert_one(new_user)
             return redirect(f"/sign-in?username={username}")
-    return render(response, "my_blog/sign_up.html", {})
-
+    return render(response, "my_blog/sign_up.html")
 
 def sign_in(response):
     authenticate_user = response.GET.get("username")
@@ -93,6 +117,12 @@ def sign_in(response):
             if u:
                 user = u[0]
                 if username == user["username"] and check_password(password, user["password"]):
+                    query_user = list(user_collection.find({"username": username}))
+                    user_id = query_user[0].get("_id")
+                    login_user = {"$set": {"logged_in": True}}
+                    user_collection.update_one({"_id": user_id}, login_user)
+                    a = {"user_username": username, "user_is_authenticated": True}
+                    response.session["user_information"] = a
                     messages.success(response, f"{username} is now logged in! Have a nice day.")
                     return redirect("/")
                 else:
