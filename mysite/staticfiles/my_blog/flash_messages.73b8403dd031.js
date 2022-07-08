f58fbde09618c4a626780fe7a5bd03b44620f690
invalid-code <jessgaming55@gmail.com>
2022-07-08 17:34:56 +0800

    python collectstatic

---
+++ mysite/staticfiles/my_blog/flash_messages.73b8403dd031.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+  let flash_messages = $("#flash_messages").children();
+  let flash_messages_container = $("#flash_messages_container").children();
+  if (flash_messages_container !== 0){
+    setTimeout(function() {
+      $("#messages").slideUp()  
+    }, 2000)
+  };
+});
